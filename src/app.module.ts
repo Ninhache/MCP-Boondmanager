74a7ca46f22da1a1f4047097fe4f2a20ebe712f0
Ninhache <mr.carambar27@gmail.com>
2026-04-09 18:09:13 +0200

    fix: register all Phase 2 modules in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,9 @@
 import { Module } from "@nestjs/common";
 import { McpModule, McpTransportType } from "@rekog/mcp-nest";
+import { AbsencesModule } from "./modules/absences/absences.module.js";
 import { BoondModule } from "./modules/boond/boond.module.js";
 import { CandidatesModule } from "./modules/candidates/candidates.module.js";
+import { CompaniesModule } from "./modules/companies/companies.module.js";
 import { OpportunitiesModule } from "./modules/opportunities/opportunities.module.js";
 import { ProjectsModule } from "./modules/projects/projects.module.js";
 import { ResourcesModule } from "./modules/resources/resources.module.js";
@@ -18,7 +20,9 @@ import { ResourcesModule } from "./modules/resources/resources.module.js";
     ResourcesModule,
     ProjectsModule,
     CandidatesModule,
+    CompaniesModule,
     OpportunitiesModule,
+    AbsencesModule,
   ],
 })
 export class AppModule {}
