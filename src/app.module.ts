8b36eb0613048918d54d7a0c7fe6da5a160cce0c
Ninhache <mr.carambar27@gmail.com>
2026-04-09 17:55:50 +0200

    feat(opportunities): add list_opportunities and get_opportunity tools (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,7 @@ import { Module } from "@nestjs/common";
 import { McpModule, McpTransportType } from "@rekog/mcp-nest";
 import { BoondModule } from "./modules/boond/boond.module.js";
 import { CandidatesModule } from "./modules/candidates/candidates.module.js";
+import { OpportunitiesModule } from "./modules/opportunities/opportunities.module.js";
 import { ProjectsModule } from "./modules/projects/projects.module.js";
 import { ResourcesModule } from "./modules/resources/resources.module.js";
 
@@ -17,6 +18,7 @@ import { ResourcesModule } from "./modules/resources/resources.module.js";
     ResourcesModule,
     ProjectsModule,
     CandidatesModule,
+    OpportunitiesModule,
   ],
 })
 export class AppModule {}
